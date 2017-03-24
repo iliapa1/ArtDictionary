5ceb36117668d47d681cf49fda41f085d8976424
georgi demirev <georgi demirev>
2017-03-24 21:34:10 +0200

    save canvas function added

--- Paint/JS/Paint-JS.js
+++ Paint/JS/Paint-JS.js
@@ -3,7 +3,7 @@ function setup() {
     var html_can = document.getElementById('canvas');
     var canvas = createCanvas(innerWidth, innerHeight);
     canvas.parent(html_can);
-    canvas.background(255,255,255)
+    canvas.background(255,255,255);
     
    
 }
@@ -20,10 +20,14 @@ function draw()
 }
 
 function update_size() {
-    radius = document.getElementById('slider').value
+    radius = document.getElementById('slider').value;
 }
 
 
 function clear1() {
     background(255,255,255);
 }
+
+function save_canvas() {
+    saveCanvas(canvas, "myCanvas", "png");
+}    
