a8c66be60347febdebb1adb8f31372bd6c230813
georgi demirev <georgi demirev>
2017-03-24 21:09:43 +0200

    canvas with draw

--- Paint/JS/Paint-JS.js
+++ Paint/JS/Paint-JS.js
@@ -1,1 +1,20 @@
+var radius = 5;
+function setup() {
+    var html_can = document.getElementById('canvas');
+    var canvas = createCanvas(innerWidth, innerHeight);
+    canvas.parent(html_can);
+    canvas.background(255,255,255)
+    
+   
+}
 
+function draw() 
+{    
+    
+
+    if(mouseIsPressed) {
+        strokeWeight(radius);
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+	
+}
