b6b4bd3a3fc1865f7c94d5834c6fd004eece86cd
iliapa1 <ilia.nikolaev.parvanov@gmail.com>
2017-03-25 10:00:48 +0200

    Preparation work to implement Oxford api with django

--- raw/Oxford.py
+++ raw/Oxford.py
@@ -6,39 +6,37 @@ import re
 #  -*- coding: utf-8 -*-
 
 pattern = ' '
-printed = 0
 
-app_id = "ac4c15fa"
-app_key = "7c0858622acda81d78ee9e746079a042"
-language = "en"
-word_id = input("Word: ")
-
-lexcat = input('What part of speech is this?')
-lexcat = lexcat.lower()
-lexcat = lexcat[0].upper() + lexcat[1:]
-
-url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower() + '/' + 'definitions'
-
-r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
-
-#print("code {}\n".format(r.status_code)) #just returns status code
-#print("text \n" + r.text)
-data = json.loads(r.text)
 #url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower()
 #r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
 #pprint(data)
 #print(data["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][2]["definitions"])
 
 
-i=0
-while(printed==0):
-    if lexcat == data["results"][0]["lexicalEntries"][i]["lexicalCategory"]:
-        definition = str(data["results"][0]["lexicalEntries"][i]["entries"][0]["senses"][0]["definitions"])
-        definition = definition[:-2]
-        definition = definition[2:]
-        definition = definition[0].upper() + definition[1:] + '.'
-        print(definition)
+def DefineWord(word_id, lexcat):
+    app_id = "ac4c15fa"
+    app_key = "7c0858622acda81d78ee9e746079a042"
+    language = "en"
 
-        printed = 1
+    lexcat = lexcat.lower()
+    lexcat = lexcat[0].upper() + lexcat[1:]
 
-    i = i+1
+    url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower() + '/' + 'definitions'
+
+    r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
+
+    #print("code {}\n".format(r.status_code)) #just returns status code
+    #print("text \n" + r.text)
+    data = json.loads(r.text)
+    i=0
+    printed = 0
+    while(printed==0):
+        if lexcat == data["results"][0]["lexicalEntries"][i]["lexicalCategory"]:
+            definition = str(data["results"][0]["lexicalEntries"][i]["entries"][0]["senses"][0]["definitions"])
+            definition = definition[:-2]
+            definition = definition[2:]
+            definition = definition[0].upper() + definition[1:]
+            print(definition)
+
+            printed = 1
+        i=i+1
