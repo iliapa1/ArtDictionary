41bcfe2046b655c13ccc363719a6b49cb81d1f24
Georgi <gdemirev78@gmail.com>
2017-03-25 19:21:32 +0200

    definition upgrade 2

--- basic template 2/Paint/JS/Words-JS.js
+++ basic template 2/Paint/JS/Words-JS.js
@@ -1,47 +1,16 @@
-var definition;
-var html_definition;
-var dictionary_noun = [];
-dictionary_noun["house"] = "A building in which people live; residence for human beings";
-dictionary_noun["city"] = "A large or important town";
-dictionary_noun["forest"] = "A large tract of land covered with trees and underbrush; woodland";
-dictionary_noun["school"] = "An institution where instruction is given, especially to persons under college age";
-dictionary_noun["living room"] = "A room in a home used, especially by a family, for leisure activities, entertaining guests, etc.; parlor.";
-dictionary_noun["computer"] = "A programmable electronic device designed to accept data, perform prescribed mathematical and logical operations at high speed, and display the results of these operations";
-dictionary_noun["tower"] = "A building or structure high in proportion to its lateral dimensions, either isolated or forming part of a building";
-dictionary_noun["crime"] = "An action or an instance of negligence that is deemed injurious to the public welfare or morals or to the interests of the state and that is legally prohibited";
-dictionary_noun["bubble"] = "A nearly spherical body of gas contained in a liquid";
-dictionary_noun["company"] = "A number of individuals assembled or associated together; group of people";
-dictionary_noun["volcano"] = "a vent in the earth's crust through which lava, steam, ashes, etc., are expelled, either continuously or at irregular intervals";
-dictionary_noun["branch"] = "A division or subdivision of the stem or axis of a tree, shrub, or other plant";
-dictionary_noun["mouth"] = "The opening through which an animal or human takes in food";
-dictionary_noun["wing"] = "Either of the two forelimbs of most birds and of bats, corresponding to the human arms, that are specialized for flight";
-dictionary_noun["pet"] = "Domesticated or tamed animal that is kept as a companion and cared for affectionately";
-dictionary_noun["jewel"] = "A cut and polished precious stone; gem";
-dictionary_noun["sun"] = "The star that is the central body of the solar system, around which the planets revolve and from which they receive light and heat";
-dictionary_noun["airplane"] = "A heavier-than-air aircraft kept aloft by the upward thrust exerted by the passing air on its fixed wings and driven by propellers, jet propulsion";
-dictionary_noun["face"] = "The front part of the head, from the forehead to the chin";
-dictionary_noun["word"] = "A unit of language, consisting of one or more spoken sounds or their written representation, that functions as a principal carrier of meaning";
-dictionary_noun["rock"] = "A large mass of stone forming a hill, cliff, promontory, or the like";
-dictionary_noun["shirt"] = "A long- or short-sleeved garment for the upper part of the body, usually lightweight and having a collar and a front opening.";
-
-
 $(document).ready(function(){
-    $("button").click(function(){
-        $("p").text(getWord());
-    });
-});   
-
-
-function getWord() {
-    key = document.getElementById("your_word").value;
     
-    if(key in dictionary_noun) {
-        definition = dictionary_noun[key];
-        html_definition = document.getElementById("definition").value = definition;
-        return definition;
-    }
-    else {
-        alert("Don't have that word!");
-    }
-}
-   
+    $("#submit_button").click(function(){
+        $("#your_word").val();
+        $("#select1").val();
+        
+        $.ajax({
+            url: "/ajax_word/?word="+$("#input1").val()+"&lexica="+$("#select1").val()
+        }).then(function(responce){
+            response=toJSON(response);
+            $("#definition").val(responce.word_definition); 
+        });
+    
+    });
+    
+});
